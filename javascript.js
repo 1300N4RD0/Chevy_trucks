aa19530893ff9b81141e9f74ad532d89c354d2e6
1300N4RD0 <144732232+1300N4RD0@users.noreply.github.com>
2023-11-29 14:14:19 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-alert('hello! welcome to my page!");
-      
-console.log('I hope you enjoy learning javascript!");
 
 function changetext() {
 document.getElementById("demo").innerHTML ="lifted trucks are better"
